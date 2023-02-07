0e08e5060c9346b01c2e5b6a533f6af3179fee53
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-02-06 23:35:03 -0700

    Add enums

---
+++ rswiki_wrapper/enums.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class BaseUrl(Enum):
+    WEIRD_GLOOP_BASE_URL = "https://api.weirdgloop.org"
+    MEDIAWIKI_BASE_URL = "https://runescape.wiki/api.php"
+    MEDIAWIKI_OSRS_BASE_URL = "https://oldschool.runescape.wiki/api.php"
+    REALTIME_WIKI_PRICE_BASE_URL = "https://prices.runescape.wiki/api/v1"
+
+
+class GameType(Enum):
+    RS = "rs"
+    OSRS = "osrs"
+    RS_FSW_2022 = "rs-fsw-2022"
+    OSRS_FSW_2022 = "osrs-fsw-2022"
+
+
+class VosDistrict(Enum):
+    AMLODD = "Amlodd"
+    CADARN = "Cadarn"
+    CRWYS = "Crwys"
+    HEFIN = "Hefin"
+    IORWERTH = "Iorwerth"
+    ITHELL = "Ithell"
+    MEILYR = "Meilyr"
+    TRAHAEARN = "Trahaearn"

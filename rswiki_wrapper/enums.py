79f4218477a0c19bbca3d32cbf83c573394bd1d0
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-02-07 18:39:27 -0700

    Shorten enum names

--- rswiki_wrapper/enums.py
+++ rswiki_wrapper/enums.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class BaseUrl(Enum):
-    WEIRD_GLOOP_BASE_URL = "https://api.weirdgloop.org"
-    MEDIAWIKI_BASE_URL = "https://runescape.wiki/api.php"
-    MEDIAWIKI_OSRS_BASE_URL = "https://oldschool.runescape.wiki/api.php"
-    REALTIME_WIKI_PRICE_BASE_URL = "https://prices.runescape.wiki/api/v1"
+    WEIRD_GLOOP = "https://api.weirdgloop.org"
+    MEDIAWIKI = "https://runescape.wiki/api.php"
+    MEDIAWIKI_OSRS = "https://oldschool.runescape.wiki/api.php"
+    REALTIME_WIKI_PRICE = "https://prices.runescape.wiki/api/v1"
 
 
 class DefaultHeaders(Enum):
